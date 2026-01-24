da649c28df71fc199cc005ecac46e0cbb4c15a7d
Forketyfork <forketyfork@icloud.com>
2026-01-24 07:32:11 +0100

    test: add TODO comment to verify SARIF upload
    This intentional issue will be removed after verifying the GitHub
    code scanning integration works correctly.

--- src/lib.zig
+++ src/lib.zig
@@ -1,3 +1,4 @@
+// TODO: remove this comment after testing SARIF upload
 pub const source = @import("source.zig");
 pub const diagnostic = @import("diagnostic.zig");
 pub const rule = @import("rule.zig");
